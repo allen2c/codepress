5e42d9472a0ff462f1f766c341cbac958a169e3a
Allen Chou <f1470891079@gmail.com>
2025-01-23 11:46:23 +0800

    Enhance CLI with flexible output and logging options
    - Add support for JSON and text output formats
    - Implement dynamic content styling via module loading
    - Enable file and stdout output configuration
    - Add verbose logging option
    - Improve CLI flexibility and user configurability

--- codepress/cli.py
+++ codepress/cli.py
@@ -1,3 +1,5 @@
+import importlib
+import json
 import logging
 import pathlib
 import typing
@@ -8,7 +10,6 @@ from logging_bullet_train import set_logger
 from codepress import LOGGER_NAME, walk_files
 
 logger = logging.getLogger(__name__)
-set_logger(LOGGER_NAME)
 
 
 @click.command()
@@ -34,12 +35,38 @@ set_logger(LOGGER_NAME)
     default=5000,
     help="Number of lines to read from each file (default: 5000)",
 )
+@click.option(
+    "--output-format",
+    type=click.Choice(["text", "json"]),
+    default="text",
+    help="Output format (default: text)",
+)
+@click.option(
+    "--output-style",
+    default="codepress:DEFAULT_CONTENT_STYLE",
+    help="Output style (default: codepress:DEFAULT_CONTENT_STYLE). Skip style if output format is json.",  # noqa: E501
+)
+@click.option(
+    "--output",
+    default=None,
+    type=click.Path(exists=False),
+    help="Output file (default: stdout)",
+)
+@click.option(
+    "--verbose/--no-verbose",
+    default=False,
+    help="Verbose output",
+)
 def main(
     path: typing.Text | pathlib.Path,
     ignore: typing.Iterable[typing.Text],
     ignore_hidden: bool,
     enable_gitignore: bool,
     truncate_lines: int,
+    output_format: typing.Literal["text", "json"],
+    output_style: typing.Text,
+    output: typing.Text | pathlib.Path | None,
+    verbose: bool,
 ):
     """
     Transforms code into clean, readable text with precision and style.
@@ -47,7 +74,15 @@ def main(
     PATH is the directory or file to process (default is current directory).
     """
 
+    if verbose:
+        set_logger(LOGGER_NAME)
+
     path = pathlib.Path(path)
+    _style_module_name, _style_var_name = output_style.split(":")
+    _style_module = importlib.import_module(_style_module_name)
+    style = getattr(_style_module, _style_var_name)
+    open_fs = open(output, "w") if output else None
+    output_content_json: typing.List[typing.Dict[typing.Text, typing.Text]] = []
 
     for file in walk_files(
         path,
@@ -56,7 +91,23 @@ def main(
         enable_gitignore=enable_gitignore,
         truncate_lines=truncate_lines,
     ):
-        logger.info(file.path)
+        if output_format == "text":
+            if open_fs:
+                open_fs.write(file.to_content(style))
+            else:
+                print(file.to_content(style))
+
+        elif output_format == "json":
+            output_content_json.append(file.__dict__())
+
+    if output_format == "json":
+        if open_fs:
+            json.dump(output_content_json, open_fs, ensure_ascii=False, indent=2)
+        else:
+            print(json.dumps(output_content_json, ensure_ascii=False, indent=2))
+
+    if open_fs:
+        open_fs.close()
 
 
 if __name__ == "__main__":
